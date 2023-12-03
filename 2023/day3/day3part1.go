fdc4b6ada28b76cbf4d908af01934891cf9a6169
Thomas William Tickle <ttickle@gmail.com>
2023-12-03 15:33:51 -0500

    Modified the day3part1.go to use the better design.

--- 2023/day3/day3part1.go
+++ 2023/day3/day3part1.go
@@ -1,21 +1,31 @@
 package main
 
 import( "bufio"
-		"fmt"
+	"fmt"
     	"os"
-		"strconv"
+	"strconv"
     	"strings"
-		"unicode"
+	"unicode"
 )
 
+// Structure for holding symbols
 type SymbolLocation struct {
-	Rune rune
-	X int
-	Y int
+	Char rune
+	Row int
+	Column int
 }
 
+// Structure for holding our possible part numbers.
+type PartNumberCandidate struct {
+	PartNumber int
+	Row int
+	StartColumn int
+	EndColumn int
+}
+
+
 func main() {
-	var sum = 0
+	// Start by getting all the lines
 	var schematic = make([][]rune, 0) 
 	var scanner = bufio.NewScanner(os.Stdin)
 	for scanner.Scan() {
@@ -26,73 +36,72 @@ func main() {
 		}
 	}
 
-	for i := 0; i < len(schematic); i++ {
-		var builder strings.Builder
-		var hasSymbol = false
-		fmt.Printf("Line %d: %s\n", i, string(schematic[i]))
-		for j := 0; j < len(schematic[i]); j++ {
-			currentRune := schematic[i][j]
-			if unicode.IsDigit(currentRune) {
-		       		builder.WriteRune(currentRune) 	
-				hasSymbol = hasSymbol || hasAdjacentSymbol(schematic, i, j)
-			}
-			if !unicode.IsDigit(currentRune) {
-				if builder.Len() > 0  {
-					var x,_ = strconv.Atoi(builder.String())
-					fmt.Printf("[%d:%t] ", x, hasSymbol)
-					if hasSymbol {
-						sum += x
-					}
-				}
-				builder.Reset()
-				hasSymbol = false
-			}
-		}
-		if builder.Len() > 0  {
-			var x,_ = strconv.Atoi(builder.String())
-			fmt.Printf("[%d:%t] ", x, hasSymbol)
-			if hasSymbol {
-				sum += x
-			}
-		}
-		fmt.Printf("\n")
 
+	// Get the Symbols and Part Number Candidates
+	var symbolLocations, partNumberCandidates = GetSymbolAndPartCandidates(schematic)
+
+	// Now check which part numbers candidates are actually part numbers and add them to the total to get the answer
+	var sum = 0
+	for _, symbolLocation := range symbolLocations {
+		for _, partNumberCandidate := range partNumberCandidates {
+			if (symbolLocation.Row - 1 <= partNumberCandidate.Row && partNumberCandidate.Row <= symbolLocation.Row + 1) {
+				 var x = IsInsideColumnRange(symbolLocation.Column, partNumberCandidate.StartColumn, partNumberCandidate.EndColumn)
+				 if x {
+					 sum = sum + partNumberCandidate.PartNumber
+				 }
+			}
+		}
 	}
+
 	fmt.Printf("Sum of parts: %d\n", sum)
 
 }
 
-func hasAdjacentSymbol(schematic [][]rune, i int, j int) bool {	
-	var startRow, endRow, startColumn, endColumn = GetSearchBounds(schematic, i, j)
-	for row := startRow; row <= endRow; row++ {
-		for column := startColumn; column <= endColumn; column++ {
-			if !unicode.IsDigit(schematic[row][column]) && schematic[row][column] != '.' {
-				return true
-			}
+func IsInsideColumnRange(column int, columnRangeStart int, columnRangeEnd int) bool {
+	for i := column - 1; i <= column + 1; i++ {
+		if columnRangeStart <= i && i <= columnRangeEnd {
+		    return true 
 		}
 	}
 	return false
 }
 
-func GetSearchBounds(schematic [][]rune, i int, j int) (int, int, int, int) {
-	var startRow = i - 1
-	if startRow < 0 {
-		startRow = 0
-	}
-	var endRow = i + 1
-	if endRow >= len(schematic) {
-		endRow = len(schematic) - 1
+
+// Returns a slice with all the symbols found, and part numbers found
+func GetSymbolAndPartCandidates(schematic [][]rune) ([]SymbolLocation, []PartNumberCandidate) {
+	var symbolLocations = make([]SymbolLocation,0)
+	var partNumberCandidates = make([]PartNumberCandidate, 0)
+	for i := 0; i < len(schematic); i++ {
+		var buffer strings.Builder
+		for j := 0; j < len(schematic[i]); j++ {
+			currentRune := schematic[i][j]
+			if !unicode.IsDigit(currentRune) {
+				if currentRune != '.' {
+					var symbolLocation = SymbolLocation{currentRune, i, j}
+					symbolLocations = append(symbolLocations, symbolLocation)
+				}
+				if (buffer.Len() > 0) {
+					var partNumber, _ = strconv.Atoi(buffer.String())
+					var partNumberCandidate  = PartNumberCandidate{PartNumber: partNumber, Row: i, StartColumn: j-buffer.Len(), EndColumn: j-1}
+					partNumberCandidates = append(partNumberCandidates, partNumberCandidate)
+				}
+				buffer.Reset()
+			}
+			if unicode.IsDigit(currentRune) {
+				buffer.WriteRune(currentRune)
+			}
+		}
+		if buffer.Len() > 0 {
+			var partNumber, _ = strconv.Atoi(buffer.String())
+			var startColumn = len(schematic[i]) - buffer.Len() - 1
+			var endColumn = len(schematic[i]) - 1 
+			var partNumberCandidate  = PartNumberCandidate{PartNumber: partNumber, Row: i, StartColumn: startColumn, EndColumn: endColumn}
+			partNumberCandidates = append(partNumberCandidates, partNumberCandidate)
+		}
+
 	}
 
-	var startColumn = j - 1
-	if startColumn < 0 {
-		startColumn = 0
-	}
 
-	var endColumn = j + 1
-	if endColumn >= len(schematic[i])  {
-		endColumn = len(schematic[i]) - 1
-	}
 
-	return startRow, endRow, startColumn, endColumn
+	return symbolLocations, partNumberCandidates
 }
