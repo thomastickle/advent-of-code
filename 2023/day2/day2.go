43adeec735a5e88c6a63b0c13944336e8f257c4e
Thomas Tickle <ttickle@gmail.com>
2023-12-19 12:12:59 -0500

    Reimplemented Part 2

--- 2023/day2/day2.go
+++ 2023/day2/day2.go
@@ -46,52 +46,47 @@ func processGame(line string) (int, bool) {
 
 var maxCountForColor = map[string]int{"red": 12, "green": 13, "blue": 14}
 
-// func main() {
-// 	sum := 0
-// 	scanner := bufio.NewScanner(os.Stdin)
-// 	for scanner.Scan() {
-// 		currentLine := scanner.Text()
-// 		sum += processGame(currentLine)
-// 	}
-// 	fmt.Printf("Sum: %d\n", sum)
-// }
 
-// func extractMinimums(hand string) map[string]int {
-// 	var minimumColorCount = make(map[string]int)
-// 	colorCountsHand := strings.Split(hand, ",")
-// 	for _, colorCount := range colorCountsHand {
-// 		var count int
-// 		var color string
-// 		fmt.Sscanf(colorCount, " %d %s", &count, &color)
-// 		minimumColorCount[color] = count
-// 	}
-// 	return minimumColorCount
-// }
+func SumOfMinimalPowerSet(games []string) int {
+	sum := 0
+	for _, game := range games {
+		minimumCubeCounts := extractMinimumForGame(game)
+		sum += computePowerSet(minimumCubeCounts)	
+	}
+	return sum
+}
 
-// func processGame(line string) int {
-// 	var game int
-// 	var minimumColorCounts = make(map[string]int)
-// 	fmt.Sscanf(line, "Game %d:", &game)
+func computePowerSet(cubeCounts map[string]int) int {
+	powerSetValue := 1
+	for _, value := range cubeCounts {
+		powerSetValue *= value
+	}
+	return powerSetValue
+}
 
-// 	hands := strings.Split(strings.Split(line, ":")[1], ";")
-// 	for _, hand := range hands {
-// 		var minimumCountsForHand = extractMinimums(hand)
-// 		minimumColorCounts = updateMinimums(minimumColorCounts, minimumCountsForHand)
-// 	}
+func extractMinimumForGame(game string) map[string]int {
+	var minimumCubeCounts map[string]int = make(map[string]int)
+	splitGamesHands := strings.Split(game, ":")
+	hands := strings.Split(splitGamesHands[1], ";")
+	for _, hand := range hands {
+		handCounts := handCounts(hand)
+		for key, value := range handCounts {
+			if value > minimumCubeCounts[key] {
+				minimumCubeCounts[key] = value
+			}
+		}
+	}
+	return minimumCubeCounts
+}
 
-// 	var cube = 1
-// 	for _, value := range minimumColorCounts {
-// 		cube *= value
-// 	}
-
-// 	return cube
-// }
-
-// func updateMinimums(current map[string]int, hand map[string]int) map[string]int {
-// 	for key, value := range hand {
-// 		if (value > current[key]) {
-// 			current[key] = value
-// 		}
-// 	}
-// 	return current
-// }
+func handCounts(hand string) map[string]int {
+	var handCounts map[string]int = make(map[string]int)
+	cubeCounts := strings.Split(hand, ",")
+	for _, cubeCount := range cubeCounts {
+		var count int
+		var color string
+		fmt.Sscanf(cubeCount, "%d %s", &count, &color)
+		handCounts[color] = count
+	}
+	return handCounts
+}
--- 2023/day2/day2_test.go
+++ 2023/day2/day2_test.go
@@ -1,6 +1,7 @@
 package day2
 
 import (
+	"aoc-2023/test"
 	"aoc-2023/util"
 	"testing"
 )
@@ -9,16 +10,28 @@ func TestPart1Test(t *testing.T) {
 	const expectedValue = 8
 	lines := util.GetLinesFromFilename("day2input_test.txt")
 	sum := SumOfValidGames(lines)
-	if (expectedValue != sum) {
+	if expectedValue != sum {
 		t.Fatalf("Expected value %d does not match actual value %d", expectedValue, sum)
 	}
 }
 
 func TestPart1(t *testing.T) {
-	const expectedValue = 2256 
+	const expectedValue = 2256
 	lines := util.GetLinesFromFilename("day2input.txt")
 	sum := SumOfValidGames(lines)
-	if (expectedValue != sum) {
+	if expectedValue != sum {
 		t.Fatalf("Expected value %d does not match actual value %d", expectedValue, sum)
 	}
+}
+
+func TestPart2Test(t *testing.T) {
+	lines := util.GetLinesFromFilename("day2input_test.txt")
+	sum := SumOfMinimalPowerSet(lines)
+	test.AssertEquals(t, 2286, sum)
+}
+
+func TestPart2(t *testing.T) {
+	lines := util.GetLinesFromFilename("day2input.txt")
+	sum := SumOfMinimalPowerSet(lines)
+	test.AssertEquals(t, 74229, sum)
 }
