62059b7745d6dbb66b027549891b807bb10ef9e6
Thomas Tickle <ttickle@gmail.com>
2023-12-20 02:12:23 -0500

    Day 14 Part 1 using the new utilities of Platform.

--- 2023/day14/day14.go
+++ 2023/day14/day14.go
@@ -1,55 +1,64 @@
 package day14
 
-type Platform struct {
-	Map [][]rune
+import (
+	"aoc-2023/day14/platform"
+)
+
+
+func CalculateLoad(platform platform.Platform) int {
+    platform.Rotate90Clockwise()
+    platform.TiltRight() 
+    return calculateLoad(platform)
 }
 
-func CalculateLoad(platformMap Platform) int {
-	tiltedPlatform := TiltNorth(platformMap)
-	return calculateLoad(tiltedPlatform)
+func calculateLoad(platform platform.Platform) int {
+    load := 0
+    
+    for i := 0; i < platform.Height; i++ {
+        for j := 0; j < platform.Width; j++ {
+            value := platform.Map[i * platform.Width + j] 
+            if value == 'O' {
+                load += j + 1
+            }
+        }
+    }
+    
+    return load
 }
 
-func calculateLoad(tiltedPlatform Platform) int {
-	load := 0
-	m := tiltedPlatform.Map
-	for i, line := range m {
-		for _, aRune := range line {
-			if aRune == 'O' {
-				load += (len(m) - i)
-			}
-		}
-	}
-	return load
-}
+// func Rotate90Clockwise(platformMap [][]rune) [][]rune {
+//     temp = platformMap
 
-func TiltNorth(platformMap Platform) Platform {
-	m := platformMap.Map
-	for i := 0; i < len(m[0]); i++ {
-		for j := 0; j < len(m); j++ {
-			z := m[j][i]
-			if z == 'O' {
-				k := j
-				for k > 0 {
-					v := m[k-1][i]
-					if v != 'O' && v != '#' {
-						x := m[k-1][i]
-						m[k-1][i] = m[k][i]
-						m[k][i] = x
-						k--
-					} else if v == 'O' || v == '#' {
-						break
-					}
-				}
-			}
-		}
-	}
-	return Platform{m}
-}
 
-func ConvertLinesToPlatform(lines []string) Platform {
-	var platformMap [][]rune
-	for _, line := range lines {
-		platformMap = append(platformMap, []rune(line))
-	}
-	return Platform{platformMap}
-}
+// }
+
+// func Tilt(platformMap [][]rune) [][]rune {
+//     for i := 0; i < len(platformMap[0]); i++ {
+//         for j := 0; j < len(platformMap); j++ {
+//             z := platformMap[j][i]
+//             if z == 'O' {
+//                 k := j
+//                 for k > 0 {
+//                     v := platformMap[k-1][i]
+//                     if v != 'O' && v != '#' {
+//                         x := platformMap[k-1][i]
+//                         platformMap[k-1][i] = platformMap[k][i]
+//                         platformMap[k][i] = x
+//                         k--
+//                     } else if v == 'O' || v == '#' {
+//                         break
+//                     }
+//                 }
+//             }
+//         }
+//     }
+//     return platformMap 
+// }
+
+// func ConvertLinesToPlatform(lines []string) Platform {
+//     var platformMap [][]rune
+//     for _, line := range lines {
+//         platformMap = append(platformMap, []rune(line))
+//     }
+//     return Platform{platformMap}
+// }
--- 2023/day14/day14_test.go
+++ 2023/day14/day14_test.go
@@ -1,21 +1,23 @@
 package day14
 
 import (
+	"aoc-2023/day14/platform"
 	"aoc-2023/test"
 	"aoc-2023/util"
 	"testing"
 )
 
-func TestDay14Part1Test(t *testing.T) {
-	lines := util.GetLinesFromFilename("day14input_test.txt")
-	platformMap := ConvertLinesToPlatform(lines)
-	load := CalculateLoad(platformMap)
-	test.AssertEquals(t, 136, load)
-}
+
+ func TestDay14Part1Test(t *testing.T) {
+ 	lines := util.GetLinesFromFilename("day14input_test.txt")
+	platform := platform.ConstructPlatform(lines)
+ 	load := CalculateLoad(platform)
+ 	test.AssertEquals(t, 136, load)
+ }
 
 func TestDay14Part1(t *testing.T) {
 	lines := util.GetLinesFromFilename("day14input.txt")
-	platformMap := ConvertLinesToPlatform(lines)
-	load := CalculateLoad(platformMap)
-	test.AssertEquals(t, 136, load)
+	platform := platform.ConstructPlatform(lines)
+	load := CalculateLoad(platform)
+	test.AssertEquals(t, 110821, load)
 }
