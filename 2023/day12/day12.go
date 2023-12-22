8561af125f00a4c7f70c031b1cf034f8f53e15f7
Thomas Tickle <ttickle@gmail.com>
2023-12-22 03:55:39 -0500

    Day 12 Complete.

--- 2023/day12/day12.go
+++ 2023/day12/day12.go
@@ -1,1 +1,123 @@
 package day12
+
+import (
+	"fmt"
+	"slices"
+	"strconv"
+	"strings"
+)
+
+type CacheKey struct {
+	SpringMap string
+	SpringValues string 
+}
+
+var cache = make(map[CacheKey]int)
+
+func GenerateKeyFromMapAndCounts(springMap []rune, springCounts []int) CacheKey{
+	return CacheKey{string(springMap), fmt.Sprintf("%v", springCounts)}
+}
+
+func CountCombinations(springMap []rune, springCounts []int) int {
+	// Return cached results.
+	key := GenerateKeyFromMapAndCounts(springMap, springCounts) 
+	value, ok := cache[key] 
+	if ok {
+		return value
+	} 
+
+	if len(springMap) == 0 {
+		if len(springCounts) == 0 {
+			return 1
+		} else {
+			return 0
+		}
+	}
+
+	if len(springCounts) == 0 {
+		if slices.Contains(springMap, '#') {
+			return 0
+		} else {
+			return 1
+		}
+	}
+
+	results := 0
+	if strings.ContainsRune(".?", springMap[0]) {
+		results += CountCombinations(springMap[1:], springCounts)
+	}
+
+	if strings.ContainsRune("#?", springMap[0]) && 
+		springCounts[0] <= len(springMap) &&
+		!slices.Contains(springMap[:springCounts[0]], '.') &&
+		(springCounts[0] == len(springMap) || springMap[springCounts[0]] != '#') {
+			// While we want to skip ahead, we need to make sure we are in bounds.	
+			bsmIndex := springCounts[0] + 1
+			if bsmIndex > len(springMap) {
+				bsmIndex = len(springMap)
+			}
+
+			results += CountCombinations(springMap[bsmIndex:], springCounts[1:])
+	}
+	cache[key] = results
+
+	return results
+}
+
+func SumCombinations(lines []string) int {
+	count := 0
+	for _, line := range lines {
+		springMap, springCounts := getMapAndValues(line)
+		count += CountCombinations([]rune(springMap), springCounts)
+	}
+	return count
+}
+
+func SumCombinationsFolded(lines []string) int {
+	count := 0
+	for i, line := range lines {
+		fmt.Println("Doing folded Combination: ", i)
+		springCounts, brokenSprings := getMapAndValues(line)
+		springCounts = unfoldSpringMap(springCounts)
+		brokenSprings = unfoldSpringCounts(brokenSprings)
+		count += CountCombinations([]rune(springCounts), brokenSprings)
+	}
+	return count
+}
+
+func unfoldSpringMap(springMap []rune) []rune {
+	var unfolded []rune
+	for i := 0; i < 5; i++ {
+		unfolded = append(unfolded, springMap... )
+		unfolded = append(unfolded, '?')
+	}
+	return unfolded[:len(unfolded) - 1]
+}
+
+func unfoldSpringCounts(springCounts []int) []int {
+	var unfolded []int
+	for i := 0; i < 5; i++ {
+		unfolded = append(unfolded, springCounts...)
+	}
+	return unfolded
+}
+
+func getMapAndValues(line string) ([]rune, []int) {
+	splitString := strings.Fields(line)
+	brokenSpringMap := splitString[0]
+	brokenSpringValues := getDamagedSpringValues(splitString[1])
+	return []rune(brokenSpringMap), brokenSpringValues
+}
+
+func getDamagedSpringValues(damageSpringString string) []int {
+	damagedSpringStrings := strings.Split(damageSpringString, ",")
+	damagedSpringValues := make([]int, len(damagedSpringStrings))
+	for i := 0; i < len(damagedSpringStrings); i++ {
+		convertedValue, err := strconv.Atoi(damagedSpringStrings[i])
+		if err != nil {
+			panic("Unable to convert value.")
+		}
+		damagedSpringValues[i] = convertedValue
+	}
+	return damagedSpringValues
+}
--- 2023/day12/day12_test.go
+++ 2023/day12/day12_test.go
@@ -1,13 +1,42 @@
 package day12
 
 import (
+	"aoc-2023/test"
 	"aoc-2023/util"
-	"fmt"
 	"testing"
 )
 
-func TestPart1Test(t *testing.T) {
-	lines := util.GetLinesFromFilename("day12test.input")
+func TestCountCombinations(t *testing.T) {
+	testString := "???.###"
+	testValues := []int{1, 1, 3}
+	result := CountCombinations([]rune(testString), testValues)
+	test.AssertEquals(t, 1, result)
 
-	fmt.Println("Lines", len(lines))
+	testString = ".??..??...?##."
+	result = CountCombinations([]rune(testString), testValues)
+	test.AssertEquals(t, 4, result)
+}
+
+func TestDay12Part1Test(t *testing.T) {
+	lines := util.GetLinesFromFilename("day12test.txt")
+	totalCombinations := SumCombinations(lines)
+	test.AssertEquals(t, 21, totalCombinations)
+}
+
+func TestDay12Part1(t *testing.T) {
+	lines := util.GetLinesFromFilename("day12input.txt")
+	totalCombinations := SumCombinations(lines)
+	test.AssertEquals(t, 7251, totalCombinations)
+}
+
+func TestDay12Part2Test(t *testing.T) {
+	lines := util.GetLinesFromFilename("day12test.txt")
+	totalCombinations := SumCombinationsFolded(lines)
+	test.AssertEquals(t, 525152, totalCombinations)
+}
+
+func TestDay12Part2(t *testing.T) {
+	lines := util.GetLinesFromFilename("day12input.txt")
+	totalCombinations := SumCombinationsFolded(lines)
+	test.AssertEquals(t, 2128386729962, totalCombinations)
 }
